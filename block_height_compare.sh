cbbc7ebd5ab2a9c9d749335e32f9b7466df00adc
shimpa <dsimpov@gmail.com>
2024-05-11 18:55:28 +0200

    Update block_height_compare.sh

--- block_height_compare.sh
+++ block_height_compare.sh
@@ -17,17 +17,25 @@ check_catching_up() {
 # Endpoints
 endpoint1="http://127.0.0.1:26657/status"
 endpoint2="https://jackal-rpc.polkachu.com/status"
+endpoint3="https://jackal-rpc.brocha.in/status"
+endpoint4="https://rpc.cosmos.directory/jackal/status"
 
 # Get block heights
 height1=$(get_block_height "$endpoint1")
 height2=$(get_block_height "$endpoint2")
+height3=$(get_block_height "$endpoint3")
+height4=$(get_block_height "$endpoint4")
 
 # Check if node is catching up
 catching_up=$(check_catching_up "$endpoint1")
 
+# Get the highest block height among the other endpoints
+highest_height=$((height2 > height3 ? height2 : height3))
+highest_height=$((highest_height > height4 ? highest_height : height4))
+
 # Compare block heights
-if [ "$height1" -ne "$height2" ]; then
-    difference=$((height1 - height2))
+if [ "$height1" -ne "$highest_height" ]; then
+    difference=$((height1 - highest_height))
     if [ "$difference" -gt 2 ] || [ "$difference" -lt -2 ]; then
         # Check if node is catching up and restart if necessary
         if [ "$catching_up" == "false" ]; then
@@ -44,5 +52,5 @@ else
 fi
 
 # Output actual block heights
-echo "Block Height - Endpoint 1: $height1"
-echo "Block Height - Endpoint 2: $height2"
+echo "Block Height - Your Node: $height1"
+echo "Block Height - Highest of the Other Endpoints: $highest_height"
